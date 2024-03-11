56a8dbf1bb7af4d15a2dce1f15b7f7833d66d52f
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-11 17:18:29 +0200

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const C = "C is fun"
-const Py = "Python is cool"
-const Js = "JavaScript is amazing"
-console.log(C + "\n" + Py + "\n" + Js)
+const C = 'C is fun';
+const Py = 'Python is cool';
+const Js = 'JavaScript is amazing';
+console.log(C + '\n' + Py + '\n' + Js);
