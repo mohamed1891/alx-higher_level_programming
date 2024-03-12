121d4c787574a5e87ef0feb97ef2595e3e921d4f
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-12 12:27:35 +0200

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    if len(sys.argv) - 1 != 4:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
