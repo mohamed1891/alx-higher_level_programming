98af4d347cb205f09fcd42d6b2f5bd0f5ec9914c
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-13 23:46:50 +0200

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+const dict = require('./101-data').dict;
+const valuesArray = Object.values(dict);
+valuesArray.sort((a, b) => a - b);
+const keysByValue = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (!keysByValue[value]) {
+    keysByValue[value] = [];
+  }
+  keysByValue[value].push(key);
+}
