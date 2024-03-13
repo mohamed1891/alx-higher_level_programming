9273665d94873936c0609bf0cd6ac0affea786f7
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-13 15:42:16 +0200

    A class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// A class Rectangle that defines a rectangle
+class Rectangle {
+  constructor(w, h) {
+    // Initialize the instance attribute 'width' with the value of 'w'
+    this.width = w;
+
+    // Initialize the instance attribute 'height' with the value of 'h'
+    this.height = h;
+  }
+}
