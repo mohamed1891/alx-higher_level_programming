ddf9a55c0cbf743e74920466078a3f463e8c4985
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-13 15:34:05 +0200

    An empty class Rectangle that defines a rectangle:

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-    // Class methods and properties can be added here
+  // Class methods and properties can be added here
 }
 
 const myRectangle = new Rectangle();
