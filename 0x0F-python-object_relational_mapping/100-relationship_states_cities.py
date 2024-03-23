530ebda09022809dd1e430e9fa173d7bbead31b5
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-23 04:41:00 +0200

    15. City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A script that creates the State “California” with
-the City “San Francisco” from the database
+A script that creates the State California with the City San Francisco
+from the database hbtn_0e_100_usa
 """
 
 from relationship_state import Base, State
@@ -11,6 +11,8 @@ from relationship_city import City
 from sys import argv
 
 if __name__ == "__main__":
+    """Function that connects to MySQL server on localhost at port 3306"""
+
     db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
                                                           argv[3])
     engine = create_engine(db)
