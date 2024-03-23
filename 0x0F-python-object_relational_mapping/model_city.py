8ef8a75b1e1e213e21edf1f6d89f3b7b1ea91801
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-23 04:38:51 +0200

    14. Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City
-an instance Base = declarative_base().
+A python file that contains class definition of City
+and instance Base = declarative_base()
 """
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
-
-Base = declarative_base()
 
 
 class City(Base):
-    """
-    City class that inherits from Base.
-    Represents the Cities table in MySQL.
-    """
-    __tablename__ = "cities"
-    id = Column(
-        Integer,
-        autoincrement=True,
-        unique=True,
-        nullable=False,
-        primary_key=True
-    )
-    name = Column(
-        String(length=128),
-        nullable=False
-    )
-    state_id = Column(
-        Integer,
-        ForeignKey('states.id'),
-        nullable=False
-    )
+    """A class that defines City insatnces"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
