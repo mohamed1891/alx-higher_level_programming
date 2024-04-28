22eacfbb04e6e77731fccb39fb005f32c9f08400
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-04-28 15:29:14 +0300

    All files project 0x11. Python - Network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" A Python script that takes in a URL and an email, 
-sends a POST request to the passed URL with the email as a parameter, 
+""" A Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)"""
 
 import urllib.request
@@ -22,4 +22,3 @@ if __name__ == "__main__":
         with urllib.request.urlopen(request) as response:
             # Decode and print response body
             print(response.read().decode("utf-8"))
-
