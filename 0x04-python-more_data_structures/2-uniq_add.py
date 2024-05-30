92618569b30627d861ab315587586118b5332e60
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-05-30 16:10:09 +0300

    All files for0x04. Python - More Data Structures: Set, Dictionary project

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-"""A function that adds all unique integers in a list (only once for each integer)
-    """
+"""A function that adds all unique integers in a list
+(only once for each integer)"""
+
+
 def uniq_add(my_list=[]):
     new_list = []
     sum = 0
