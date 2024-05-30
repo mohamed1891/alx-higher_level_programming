f5cccb62d94ff8072543bd7dd08c2f6b817b1f6c
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-05-30 16:21:59 +0300

    All files for0x04. Python - More Data Structures: Set, Dictionary project

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
 
 def roman_to_int(roman_string):
-    # Define the mapping of Roman numerals to their integer values
-    roman_values = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    # Initialize the result
-    result = 0
-
-    # Check if the input is valid
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string:
         return 0
 
-    # Iterate through the Roman string from left to right
-    for i in range(len(roman_string)):
-        current_value = roman_values.get(roman_string[i], 0)
-        next_value = roman_values.get(roman_string[i + 1], 0)
-        if (i + 1) < len(roman_string) else 0
+    if not isinstance(roman_string, str):
+        return 0
 
-        # If the current value is less than the next value, subtract it
-        if current_value < next_value:
-            result -= current_value
-        else:
-            result += current_value
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
-    return result
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
