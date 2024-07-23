cfffa80d7fdd219c63183e94611a529deb16fbe6
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-07-23 20:18:53 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 
 def list_division(ls_1, ls_2, list_length):
-    result = []
+    new_list = []
     for i in range(list_length):
         try:
-
+            # Check if the index is out of range for the lists
             if i >= len(ls_1) or i >= len(ls_2):
-                print("out of range")
-                result.append(0)
-            else:
+                raise IndexError  # This will be caught by the IndexError block
 
-                if not isinstance(ls_1[i], (int, float)) or not isinstance(ls_2[i], (int, float)):
-                    print("wrong type")
-                    result.append(0)
-                else:
+            # Check if the element in ls_1 is a number
+            if not isinstance(ls_1[i], (int, float)):
+                print("wrong type")
+                new_list.append(0)
+                continue  # Skip the rest of the loop for this iteration
 
-                    try:
-                        division_result = ls_1[i] / ls_2[i]
-                        result.append(division_result)
-                    except ZeroDivisionError:
-                        print("division by 0")
-                        result.append(0)
-        except Exception as e:
+            # Check if the element in ls_2 is a number
+            if not isinstance(ls_2[i], (int, float)):
+                print("wrong type")
+                new_list.append(0)
+                continue  # Skip the rest of the loop for this iteration
 
-            print(f"An unexpected error occurred: {e}")
-            result.append(0)
-        finally:
-
-            continue
-    
-    return result
+            # Perform the division
+            new_list.append(ls_1[i] / ls_2[i])
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+    return new_list
