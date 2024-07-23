3576824e781fe1f1f7e139a4737ac16fa28f3952
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-07-23 20:21:37 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
 
-def list_division(ls_1, ls_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            # Check if the index is out of range for the lists
-            if i >= len(ls_1) or i >= len(ls_2):
-                raise IndexError  # This will be caught by the IndexError block
-
-            # Check if the element in ls_1 is a number
-            if not isinstance(ls_1[i], (int, float)):
-                print("wrong type")
-                new_list.append(0)
-                continue  # Skip the rest of the loop for this iteration
-
-            # Check if the element in ls_2 is a number
-            if not isinstance(ls_2[i], (int, float)):
-                print("wrong type")
-                new_list.append(0)
-                continue  # Skip the rest of the loop for this iteration
-
-            # Perform the division
-            new_list.append(ls_1[i] / ls_2[i])
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+            new_list.append(my_list_1[i] / my_list_2[i])
         except TypeError:
             print("wrong type")
             new_list.append(0)
@@ -32,3 +17,4 @@ def list_division(ls_1, ls_2, list_length):
             print("out of range")
             new_list.append(0)
     return new_list
+
