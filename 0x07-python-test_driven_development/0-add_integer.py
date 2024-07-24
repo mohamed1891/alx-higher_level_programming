3c426f1f75d678c93b49c3361c0c5ef5fd5265ac
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-07-24 19:17:17 +0300

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
+"""
+1. add_integer
+Add two integers
+"""
 
 
 def add_integer(a, b=98):
-    """ function to add two integer.
-    Args:
-    a : first int
-    b : sec int
-    """
-
-    if type(a) not in [int, float]:
+    """Add two integers"""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
